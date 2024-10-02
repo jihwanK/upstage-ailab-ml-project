c77b66519994e8df9c71344b56a7c71e0ce083f4
jihwanK <computerandgyein@gmail.com>
2024-10-03 01:20:26 +0900

    [Feature] Add url crawler

---
+++ src/crawler/url_crawler.py
@@ -0,0 +1,37 @@
+import time
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+cat_nums = [
+    10000010011, 10000010002, 10000010012, 10000010006, 10000010008,
+    10000010007, 10000010005, 10000010004, 10000010003, 10000020001,
+    10000020002, 10000020003, 10000020005, 10000020004, 10000030007,
+    10000030005, 10000030006, 10000010001, 10000010009, 10000010010,
+]
+
+cat_names = [
+    "선케어", "메이크업", "네일", "미용소품", "더모코스메틱", "맨즈케어", "향수-디퓨저", "헤어케어",
+    "바디케어", "건강식품", "푸드", "구강용품", "헬스-건강용품", "여성-위생용품", "컴포트웨어",
+    "리빙-펫", "취미-팬시", "스킨케어", "마스크팩", "클렌징"
+]
+
+product_url_dict = {}
+for i, cat_num in enumerate(cat_nums):
+    url_list = []
+    page = f"https://www.oliveyoung.co.kr/store/main/getBestList.do?dispCatNo=900000100100001&fltDispCatNo={cat_num}"
+    print(f"[START] Crawl {cat_names[i]} URLs")
+
+    html = requests.get(page.format(cat_num), timeout=100).text
+    soup = BeautifulSoup(html, "html.parser")
+
+    products = soup.find_all("div", attrs={"class": "prd_info"})
+    for product in products:
+        url_list.append(product.find("a").attrs["href"])
+    product_url_dict[cat_names[i]] = url_list
+    time.sleep(2)
+
+    print(f"[FINISH] Crawl {cat_names[i]} URLs")
+
+pd.DataFrame(product_url_dict).to_csv("../../data/product_url.csv", index=False)
