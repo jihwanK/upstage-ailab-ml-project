3d6190b9ffa1348071425cd1ed674a85ee346217
jihwanK <computerandgyein@gmail.com>
2024-10-03 12:34:36 +0900

    [Update] Make the crawler more robust
    Add WebDriverWait with visibility_of_all_elements_located

--- src/crawler/crawler_v2.py
+++ src/crawler/crawler_v2.py
@@ -1,3 +1,4 @@
+import os
 import re
 import time
 from collections import defaultdict
@@ -14,12 +15,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException, StaleElementReferenceException
 
+
 logging.basicConfig(filename="../../logs/crawler.log", filemode='a', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-logging.getLogger(__name__).setLevel(logging.INFO)
+# logging.getLogger(__name__).setLevel(logging.INFO)
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=1920x1080")
 chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--disable-infobars')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-blink-features=AutomationControlled')
 service = Service()
@@ -33,9 +37,9 @@ def extract_product_info(driver, url):
         brand_name = product_info.find_element(By.CSS_SELECTOR, "p.prd_brand").text.strip()
         product_name = product_info.find_element(By.CSS_SELECTOR, "p.prd_name").text.strip()
         product_flags = [flag.text.strip() for flag in product_info.find_elements(By.CSS_SELECTOR, "p.prd_flag span")]
-        
+
         logging.info("[extract_product_info] FINISH")
-        
+
         return {
             "brand_name": brand_name,
             "product_name": product_name,
@@ -45,13 +49,13 @@ def extract_product_info(driver, url):
 
     except (NoSuchElementException, TimeoutException, StaleElementReferenceException) as e:
         print("NoSuchElementException, TimeoutException extracting product info")
-        logging.error(f"Error extracting product info from {url}: {e}")
+        logging.error("Error extracting product info from %s \n%s", url, e)
         error_list["url"].append(url)
         error_list["reason"].append("Product info NoSuchElementException, TimeoutException")
         return {"brand_name": None, "product_name": None, "product_flag": None}
     except Exception as e:
         print("Error extracting product info")
-        logging.error(f"Error extracting product info from {url}: {e}")
+        logging.error("Error extracting product info from %s \n%s", url, e)
         error_list["url"].append(url)
         error_list["reason"].append("Product info Error")
         return {"brand_name": None, "product_name": None, "product_flag": None}
@@ -66,7 +70,7 @@ def extract_ingredients(driver, url):
         element.click()
 
         WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "div#artcInfo > dl.detail_info_list"))
+            EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div#artcInfo > dl.detail_info_list"))
         )
 
         product_info_list = driver.find_elements(By.CSS_SELECTOR, "div#artcInfo > dl.detail_info_list")
@@ -78,14 +82,14 @@ def extract_ingredients(driver, url):
                 return product_info.find_element(By.TAG_NAME, "dd").text.strip()
 
     except (NoSuchElementException, TimeoutException, StaleElementReferenceException) as e:
-        print(f"NoSuchElementException, TimeoutException extracting ingredients")
-        logging.error(f"Specific error (NoSuchElementException or TimeoutException) in extract_ingredients: {e}")
+        print("NoSuchElementException, TimeoutException extracting ingredients")
+        logging.error("Specific error (NoSuchElementException or TimeoutException) in extract_ingredients \n%s", e)
         error_list["url"].append(url)
         error_list["reason"].append("ingredients error")
         return None
     except Exception as e:
-        print(f"Error extracting ingredients")
-        logging.error(f"Error extracting ingredients: {e}")
+        print("Error extracting ingredients")
+        logging.error("Error extracting ingredients \n%s", e)
         error_list["url"].append(url)
         error_list["reason"].append("ingredients error")
         return None
@@ -104,7 +108,7 @@ def extract_reviews(driver, product_info_dict):
         element.click()
 
         WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "div.review_list_wrap"))
+            EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div.review_list_wrap"))
         )
 
         review_cnt_elem = driver.find_element(By.CSS_SELECTOR, "ul.prd_detail_tab > li#reviewInfo > a > span")
@@ -116,13 +120,13 @@ def extract_reviews(driver, product_info_dict):
 
     except (NoSuchElementException, TimeoutException, StaleElementReferenceException) as e:
         print("NoSuchElementException or TimeoutException extracting review count or preparing reviews")
-        logging.error(f"Specific error (NoSuchElementException or TimeoutException) in extract_reviews: {e}")
+        logging.error("Specific error (NoSuchElementException or TimeoutException) in extract_reviews \n%s", e)
         error_list["url"].append(product_info_dict["url"])
         error_list["reason"].append("review count error")
         return review_info_dict, review_cnt
     except Exception as e:
         print("Error extracting review count or preparing reviews")
-        logging.error(f"Error extracting review count or preparing reviews: {e}")
+        logging.error("Error extracting review count or preparing reviews \n%s", e)
         error_list["url"].append(product_info_dict["url"])
         error_list["reason"].append("review count error")
         return review_info_dict, review_cnt
@@ -139,11 +143,12 @@ def extract_reviews(driver, product_info_dict):
             review_list = driver.find_elements(By.CSS_SELECTOR, "div.review_list_wrap > ul#gdasList > li")
 
             for idx, review in enumerate(review_list):
-                # print(f"Crawling {idx}th review")
-
                 try:
                     WebDriverWait(driver, 10).until(
-                        EC.presence_of_element_located((By.CSS_SELECTOR, "div.info > div.user > a"))
+                        EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div#Contents div.review_wrap div.info > div.user > p.info_user"))
+                    )
+                    WebDriverWait(driver, 10).until(
+                        EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div#Contents div.review_wrap div.review_cont > div.txt_inner"))
                     )
                     user_info = review.find_element(By.CSS_SELECTOR, "div.info")
                     user_link = user_info.find_element(By.TAG_NAME, "a")
@@ -153,13 +158,19 @@ def extract_reviews(driver, product_info_dict):
                     try:
                         user_skin_type = [element.text.strip() for element in user_info.find_elements(By.CSS_SELECTOR, "p.tag > span")]
                     except (NoSuchElementException, TimeoutException, StaleElementReferenceException) as e:
-                        logging.info("user_skin is missing")
+                        logging.error("user_skin is missing")
+                        print("user_skin is missing")
+                        error_list["url"].append(product_info_dict["url"])
+                        error_list["reason"].append("user skin error")
                         user_skin_type = []
                     try:
                         user_tag_list = [ele.text.strip() for ele in user_info.find_elements(By.CSS_SELECTOR, "div.badge > a.point_flag")]
                         user_tag = ",".join(user_tag_list)
                     except (NoSuchElementException, TimeoutException, StaleElementReferenceException) as e:
-                        logging.info("user_tag is missing")
+                        logging.error("user_tag is missing")
+                        print("user_tag is missing")
+                        error_list["url"].append(product_info_dict["url"])
+                        error_list["reason"].append("user tag error")
                         user_tag = None
 
                     review_info = review.find_element(By.CSS_SELECTOR, "div.review_cont")
@@ -168,7 +179,8 @@ def extract_reviews(driver, product_info_dict):
                     try:
                         purchase_channel = review_info.find_element(By.CSS_SELECTOR, "div.score_area > span.ico_offlineStore").text.strip()
                     except (NoSuchElementException, TimeoutException, StaleElementReferenceException) as e:
-                        logging.info("purchase_channel is missing")
+                        # logging.error("purchase_channel is missing")
+                        # print("purchase_channel is missing")
                         purchase_channel = "온라인"
                     item_option_elements = review_info.find_elements(By.CSS_SELECTOR, "p.item_option")
                     item_option = item_option_elements[0].text.strip() if item_option_elements else None
@@ -201,24 +213,24 @@ def extract_reviews(driver, product_info_dict):
 
                 except (NoSuchElementException, TimeoutException, StaleElementReferenceException) as e:
                     print(f"NoSuchElementException, TimeoutException extracting review[{idx}] on page {page}")
-                    logging.error(f"Specific error in review[{idx}] on page {page} for {product_info_dict['url']}: {e}")
+                    logging.error("Specific error in review[%d] on page %d for %s \n%s", idx, page, product_info_dict["url"], e)
                     error_list["url"].append(product_info_dict["url"])
                     error_list["reason"].append(f"extracting review error review[{idx}] on page {page}")
                 except Exception as e:
                     print(f"Error extracting review[{idx}] on page {page}")
-                    logging.error(f"Error extracting review[{idx}] on page {page}: {e}")
+                    logging.error("Error extracting review[%d] on page %d \n%s", idx, page, e)
                     error_list["url"].append(product_info_dict["url"])
                     error_list["reason"].append(f"extracting review error review[{idx}] on page {page}")
 
 
         except (NoSuchElementException, TimeoutException, StaleElementReferenceException) as e:
             print(f"NoSuchElementException, TimeoutException navigating to page {page}")
-            logging.error(f"Specific error navigating to page {page} for {product_info_dict['url']}: {e}")
+            logging.error("Specific error navigating to page %d for %s \n%s", page, product_info_dict["url"], e)
             error_list["url"].append(product_info_dict["url"])
             error_list["reason"].append(f"navigating to page error on {page}")
         except Exception as e:
             print(f"Error navigating to page {page}")
-            logging.error(f"Error navigating to page {page}: {e}")
+            logging.error("Error navigating to page %d \n%s", page, e)
             error_list["url"].append(product_info_dict["url"])
             error_list["reason"].append(f"navigating to page error on {page}")
 
@@ -233,6 +245,7 @@ def scrape_product_reviews(product_url_dict):
 
     for cat_name in tqdm(product_url_dict, desc="Category", position=0):
         product_info_dict = defaultdict(list)
+        product_num = 0
 
         for url in tqdm(product_url_dict[cat_name], desc="URL", position=1, leave=False):
             try:
@@ -242,6 +255,7 @@ def scrape_product_reviews(product_url_dict):
 
                     product_info = extract_product_info(driver, url)
                     ingredients = extract_ingredients(driver, url)
+                    reviews, review_cnt, overall_rating = extract_reviews(driver, product_info)
 
                     product_info_dict["category"].append(cat_name)
                     product_info_dict["product_name"].append(product_info["product_name"])
@@ -250,39 +264,43 @@ def scrape_product_reviews(product_url_dict):
                     product_info_dict["url"].append(url)
                     product_info_dict["ingredients"].append(ingredients)
 
-                    reviews, review_cnt, overall_rating = extract_reviews(driver, product_info)
                     product_info_dict["review_cnt"].append(review_cnt)
                     product_info_dict["overall_rating"].append(overall_rating)
 
                     reviewer_list.update(reviews["user_code"])
 
                     time.sleep(2)
-                    
+
                     try:
-                        pd.DataFrame(reviews).to_csv(f"../../data/reviews/{cat_name}_{product_info['product_name']}_reviews.csv", index=False)
+                        os.makedirs(f"../../data/reviews/{cat_name}", exist_ok=True)
+                        pd.DataFrame(reviews).to_csv(f"../../data/reviews/{cat_name}/{cat_name}_{product_num}_reviews.csv", index=False)
+                        logging.info("Safely saved %s", {product_info['product_name']})
                     except Exception as e:
                         print(f"Error saving data for category {cat_name}_{product_info['product_name']}")
-                        logging.error(f"Error saving data for category {cat_name}_{product_info['product_name']}: {e}")
+                        logging.error("Error saving data for category %s_%s \n%s", cat_name,product_info['product_name'], e)
+                    finally:
+                        product_num += 1
 
             except Exception as e:
                 print(f"Error processing URL {url} in category {cat_name}")
-                logging.error(f"Error processing URL {url} in category {cat_name}: {e}")
-        
+                logging.error("Error processing URL %s in category %s \n%s", url, cat_name, e)
+
         try:
             pd.DataFrame(product_info_dict).to_csv(f"../../data/products/{cat_name}.csv", index=False)
         except Exception as e:
             print(f"Error saving data for category {cat_name}")
-            logging.error(f"Error saving data for category {cat_name}: {e}")
+            logging.error("Error saving data for category %s \n%s", cat_name, e)
+
     try:
         pd.DataFrame(list(reviewer_list), columns=["user_code"]).to_csv("../../data/reviewers.csv", index=False)
     except Exception as e:
-        print(f"Error saving reviewer data")
-        logging.error(f"Error saving reviewer data: {e}")
+        print("Error saving reviewer data")
+        logging.error("Error saving reviewer data \n%s", e)
 
 
-
+# main function
 if __name__ == "__main__":
-    product_url_dict = pd.read_csv("product_url.csv").to_dict("list")
+    product_url_dict = pd.read_csv("../../data/product_url.csv").to_dict("list")
     try:
         # scrape_product_reviews({"선케어": product_url_dict["선케어"]})
         # del product_url_dict["선케어"]
