86daa07f35acef34615e6e4fecf1a8b6c6827b23
jihwanK <computerandgyein@gmail.com>
2024-10-11 18:20:20 +0900

    [Update] Update api

--- src/mlops/api/img_url.py
+++ src/mlops/api/img_url.py
@@ -11,9 +11,12 @@ def get_image_urls(items, caches):
             print(product_name2id[item[0]])
             html = requests.get(page.format(product_id=product_name2id[item[0]]), timeout=100).text
             soup = BeautifulSoup(html, "html.parser")
-            url = soup.find("div", attrs={"class": "prd_img"}).find("img").attrs["src"]
-            caches[item] = url
-            urls.append(url)
+            try:
+                url = soup.find("div", attrs={"class": "prd_img"}).find("img").attrs["src"]
+                caches[item] = url
+                urls.append(url)
+            except Exception:
+                urls.append(None)
         else:
             urls.append(caches[item])
 
